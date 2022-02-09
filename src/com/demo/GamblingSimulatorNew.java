96518405a34a78d9c5605ad47aff75d67d90bbd8
first_name <ss329222@gmail.com>
2022-02-09 13:04:27 +0530

    main

---
+++ src/com/demo/GamblingSimulatorNew.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+public class GamblingSimulatorNew 
+{
+	
+
+	public class GamingSimulator {
+		 int STAKE_PER_DAY = 100;
+		 int BET_PER_GAME = 1;
+
+		public static void main(String[] args) {
+			System.out.println("Welcome to Gambling Simulator");
+		}
+	}
+
+}
